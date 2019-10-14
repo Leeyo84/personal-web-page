91b4148bc63ef67bdedf5908c1b483debd6de32a
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-14 13:02:42 -0600

    added pupose, audience and Goal to milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Personal Web Page</title>
+</head>
+
+<body>
+	<p>
+		Purpose
+	</p>
+	<p>
+		Audience
+	</p>
+	<p>
+		Goal
+	</p>
+</body>
+</html>
