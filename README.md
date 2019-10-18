be0d0bd8ec0c96b188c0193e35ec6b5f31ceb4f2
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-17 20:20:33 -0600

    added shor discription to readme.md and added documentation for Phase 1 to public_html/documetation/milesotne-1.php

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # personal-web-page
 portfolio
-Personal Web Page Portfolio. Will show skill-sets for Leeyo84.
+Personal Web Page Portfolio. Will show skill-sets for Leeyo84 for potential employers to visit. 
--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,14 +6,42 @@
 </head>
 
 <body>
-	<p>
-		Purpose
-	</p>
-	<p>
-		Audience
-	</p>
-	<p>
-		Goal
-	</p>
+	<div>
+		<h2>My Personal Webpage</h2>
+		<h4>Purpose</h4>
+			<p>This project will be a professional portfolio website to showcase my Web Development Skillset.</p>
+		<h4>Audience</h4>
+			<p> This site is geared toward hiring professionals in the Tech industry.</p>
+		<h4>Goal</h4>
+			<p>The primary goal of this site is to provide users with an easy way to contact me directly for potential collaborations. </p>
+			<p>Secondly, I want to promote myself and give the user an opportunity to learn more about my motivations and what I
+				have to offer.</p>
+	</div>
+
+	<div>
+		<h2>Persona</h2>
+		<h3>Lillian</h3>
+		<h4>age:35</h4>
+			<p><strong>Profession:</strong> Founder of startup geared at the betterment of their community, looking for a Front-End Web Developer.<br>
+				They offer outreach and resources for women interested in learning how to code for a better future.</p>
+
+			<p><strong>Tech:</strong> Android phone for work and personal use. Two large monitors at work, and Dell laptop at home.<br>
+				uses cloud for a seamless transition from her office, home, and while on the go.</p>
+
+			<p><strong>Attitudes and Behaviors:</strong> Extremely busy, in and out of meetings, limited social life on weekends due to outreach and networking on her time out of the office.<br>
+				Isn't looking to spend too much researching new potential hires; needs information presented in a simple and concise way that gets the message across without too much effort on her behalf, <br>
+				and the ability to find the info easily and viewable on all her devices.</p>
+
+			<p><strong>Frustrations and Needs:</strong> Lillian needs someone that requires little instruction and is dependable. <br>
+				she does not have time to spend on training 1 on 1 but can offer support to the right person. <br>
+				Since the startup is still fairly new, although established, she's open to hiring someone with a Bootcamp certificate in order to save money.</p>
+
+			<p><strong>Goals:</strong> Lillian needs to hire someone with an understanding of her goal, and someone willing to help her achieve success.</p>
+
+			<p><strong>User Story:</strong> As a user, I need to be able to see a potential candidates qualifications quickly and contact them easily.</p>
+
+
+	</div>
+
 </body>
 </html>
