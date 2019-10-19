4830b3f517160aebbbbc66dd8e2be19619ece649
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-19 08:54:44 -0600

    fixed some typos in content

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -28,15 +28,15 @@
 			<p><strong>Tech:</strong> Android phone for work and personal use. Two large monitors at work, and Dell laptop at home.<br>
 				uses cloud for a seamless transition from her office, home, and while on the go.</p>
 
-			<p><strong>Attitudes and Behaviors:</strong> Extremely busy, in and out of meetings, limited social life on weekends due to outreach and networking on her time out of the office.<br>
-				Isn't looking to spend too much researching new potential hires; needs information presented in a simple and concise way that gets the message across without too much effort on her behalf, <br>
-				and the ability to find the info easily and viewable on all her devices.</p>
+			<p><strong>Attitudes and Behaviors:</strong> Extremely busy, in and out of meetings, limited social life on weekends due to outreach and networking.<br>
+				Isn't looking to spend too much time researching new potential hires; needs information presented in a simple and concise way that gets the message across without too much effort on her behalf. <br>
+				Wants the ability to find information that is viewable on all her devices and is easy to navigate.</p>
 
 			<p><strong>Frustrations and Needs:</strong> Lillian needs someone that requires little instruction and is dependable. <br>
-				she does not have time to spend on training 1 on 1 but can offer support to the right person. <br>
+				She does not have time to trian someone 1 on 1 but can offer support to the right person. <br>
 				Since the startup is still fairly new, although established, she's open to hiring someone with a Bootcamp certificate in order to save money.</p>
 
-			<p><strong>Goals:</strong> Lillian needs to hire someone with an understanding of her goal, and someone willing to help her achieve success.</p>
+			<p><strong>Goals:</strong> Lillian needs to hire someone with an understanding of her goal, thats willing to help her achieve success.</p>
 
 			<p><strong>User Story:</strong> As a user, I need to be able to see a potential candidates qualifications quickly and contact them easily.</p>
 
