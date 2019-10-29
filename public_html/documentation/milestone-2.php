2beeb020afedaf9658850d2c2bb74065a8101bac
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-29 14:27:40 -0600

    corrected errors caught by validator, changed /br to br

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,25 +9,25 @@
 	<div>
 		<h3>Welcome Page</h3>
 		<p>This page will be simple and have the feel of a business card. It will focus on introducing myself as a FullStack Web Developer.
-			It will have a sneak peek of the following section to entice the viewer to scroll down to see more. 	</br>
-			It will also contain a “navbar” where the viewer can jump directly to the desired section without having to scroll the entire page. 	</br>
+			It will have a sneak peek of the following section to entice the viewer to scroll down to see more. <br>
+			It will also contain a “navbar” where the viewer can jump directly to the desired section without having to scroll the entire page. <br>
 			The background will be a solid color that gradually changes to white as you scroll to the second section.
 		</p>
 		<h3>About Me</h3>
-		<p>The second section of the webpage will go more into detail about my skillsets, including programming languages,	</br>
-			and will also give the user a larger picture and look into my personality. I want to make it visually dynamic.	</br>
+		<p>The second section of the webpage will go more into detail about my skillsets, including programming languages,<br>
+			and will also give the user a larger picture and look into my personality. I want to make it visually dynamic.	<br>
 			I want to create movement with the line that runs from section one(welcome page) to section two (about me).
 		</p>
 		<h3>Portfolio</h3>
-		<P>The Third section will allow me to display my capstone project and add links to my GitHub and linked in profile.	</br>
-			I want to continue the same movement and fell of the second section, using large circles with images of my capstone project.	</br>
+		<P>The Third section will allow me to display my capstone project and add links to my GitHub and linked in profile.<br>
+			I want to continue the same movement and fell of the second section, using large circles with images of my capstone project.<br>
 			I can later add more project links, creating a collection of circles.
 		</P>
 		<h3>Contact Me</h3>
-		<p>The fourth and last section of the page, will be a simple contact form. Again following the same movement from the lines in previous sections.	</br>
+		<p>The fourth and last section of the page, will be a simple contact form. Again following the same movement from the lines in previous sections.<br>
 			It will encourage the viewer to “Hire me” by contacting me directly form my page.
 		</p>
-		</br>
+		<br>
 	</div>
 	<div>
 		<h3>Wirefromes</h3>
