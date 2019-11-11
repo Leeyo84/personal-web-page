dc795fe2df9c35e7c15c380c1670dfe7ecdab56e
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-11-11 15:03:52 -0700

    set up config for contact form

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,3 +1,14 @@
-function f() {
-	
-}
+$(document).ready(function(){
+	var scroll_pos = 0;
+	$(document).scroll(function() {
+		scroll_pos = $(this).scrollTop();
+		if(scroll_pos > 300) {
+			$("body").css('background-color', 'blue');
+		} else {
+			$("body").css('background-color', 'red');
+		}
+	});
+});
+
+
+onscroll fade to transparent
