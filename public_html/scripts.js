7d02dfad1409e732966f075cf637a27180a4602b
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-11-11 16:53:25 -0700

    adding styling to contact form and card deck

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -1,14 +1,4 @@
-$(document).ready(function(){
-	var scroll_pos = 0;
-	$(document).scroll(function() {
-		scroll_pos = $(this).scrollTop();
-		if(scroll_pos > 300) {
-			$("body").css('background-color', 'blue');
-		} else {
-			$("body").css('background-color', 'red');
-		}
-	});
-});
+
 
 
 onscroll fade to transparent
