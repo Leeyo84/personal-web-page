198cc1ae27fb3b9ed8de5c92e487cba3c39dea7a
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-11-27 16:08:59 -0700

    finally got background to change on scroll... my page is awesome

--- public_html/styles/scripts.js
+++ public_html/styles/scripts.js
@@ -8,3 +8,24 @@ function closeNav() {
 	document.getElementById("myNav").style.width = "0%";
 }
 
+//changes background color on scroll
+function changeColor(){
+	if(window.pageYOffset < 1000){
+		document.body.style.backgroundColor = '#0073AA'
+	}
+	if(window.pageYOffset < 2000 && window.pageYOffset > 1000){
+		document.body.style.backgroundColor = '#ffffff'
+	}
+	if(window.pageYOffset < 3000 && window.pageYOffset > 2000){
+		document.body.style.backgroundColor = '#ffffff'
+	}
+	if(window.pageYOffset < 4000 && window.pageYOffset > 3000){
+		document.body.style.backgroundColor = '#0073AA'
+	}
+	// if(window.pageYOffset < 5000 && window.pageYOffset > 4000){
+	// 	document.body.style.backgroundColor = '#0073AA'
+	// }
+	// if(window.pageYOffset > 5000){
+	// 	document.body.style.backgroundColor = '#ffffff'
+	// }
+}
